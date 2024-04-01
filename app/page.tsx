597cb6b1983aac25bf28309add1f32c2c25af66d
stefanpython <stefanhtml@yahoo.com>
2024-04-01 17:56:40 +0300

    Create homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import Link from "next/link";
 
 export default function Home() {
@@ -5,9 +6,28 @@ export default function Home() {
     <main className="py-6">
       <div className="container max-w-2xl m-auto px-4">
         <nav className="flex space-x-4 text-xl">
-          <Link href="/about">About</Link>
+          <Link href="/">About</Link>
           <Link href="posts">Posts</Link>
         </nav>
+      </div>
+
+      <div className="container max-w-2xl m-auto px-4 py-12">
+        <h1 className="text-2xl font-semibold">
+          Hey, I'm a Senior Software Engineer at Company. I enjoy working with
+          Next.js and crafting beautiful front-end experiences.
+        </h1>
+
+        <p className="py-6 text-lg">
+          This portfolio is built with Next.js and a library called next-mdx. It
+          allows you to write Markdown and focus on the content of your
+          portfolio.
+        </p>
+
+        <p className="text-lg">Deploy your own in a few minutes.</p>
+      </div>
+
+      <div className="container max-w-4xl m-auto px-4 py-12">
+        <Image src="/home.jpg" alt="office image" width={1000} height={900} />
       </div>
     </main>
   );
