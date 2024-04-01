9beebbaa69cd8c6f25093c83b3b14b3c7e0f4ac4
stefanpython <stefanhtml@yahoo.com>
2024-04-01 18:18:42 +0300

    Create placeholder data

---
+++ app/lib/palceholder-data.js
@@ -0,0 +1,41 @@
+const posts = [
+  {
+    id: 1,
+    title: "This is the first title",
+    description:
+      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. At quo sapiente atque!",
+    content:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium exercitationem error animi aspernatur, quibusdam inventore a ab temporibus perspiciatis. Id totam praesentium consequuntur. Aperiam, animi impedit vitae laboriosam quasi earum a magni repellendus. Dolore quo quaerat vitae unde temporibus iure.",
+  },
+
+  {
+    id: 2,
+    title: "Something something other title",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem illum non veniam.",
+    content:
+      " Lorem ipsum dolor sit, amet consectetur adipisicing elit. In dolorum voluptates eius minus porro, neque magnam tempora voluptatum earum! Eius est harum eaque aspernatur maxime vitae, vero repellendus debitis consectetur mollitia voluptatibus, culpa repellat autem libero dolorem ratione eum non.",
+  },
+
+  {
+    id: 3,
+    title: "NextJs lorem lorem title",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil expedita culpa explicabo!",
+    content:
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Modi molestiae culpa consequatur deleniti quasi perspiciatis vero dolorem ea sed ratione dignissimos delectus minus architecto molestias corporis quibusdam ad nobis, sequi excepturi quam labore? Magni cupiditate ea veniam nulla. Numquam, rerum.",
+  },
+
+  {
+    id: 4,
+    title: "React vs reactNative title",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum deserunt id quia.",
+    content:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptas, quis nobis hic omnis quo perspiciatis. Porro est minima eius dignissimos voluptates facere sapiente omnis at corrupti natus tempore quas, suscipit aliquam molestiae iure, dolores minus dolorem maiores fuga quisquam voluptatum?",
+  },
+];
+
+module.exports = {
+  posts,
+};
