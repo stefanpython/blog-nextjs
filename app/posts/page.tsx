c5ecc6be91505489f7661655219feac62c4d621e
stefanpython <stefanhtml@yahoo.com>
2024-04-02 16:24:26 +0300

    Minor style edit

--- app/posts/page.tsx
+++ app/posts/page.tsx
@@ -3,10 +3,13 @@ import Link from "next/link";
 
 export default function Posts() {
   return (
-    <div className="container max-w-2xl m-auto px-4 py-10">
+    <div className="container max-w-2xl m-auto px-4 py-10 ">
       {posts.map((post) => (
         <Link key={post.id} href={`/posts/${post.id}`}>
-          <div key={post.id} className="post py-4">
+          <div
+            key={post.id}
+            className="post py-4 hover:bg-slate-100 p-1 rounded-sm"
+          >
             <h3 className="text-2xl font-medium">{post.title}</h3>
             <p>{post.description}</p>
             <span className="flex text-gray-500">{post.date}</span>
