89544c34e2850b948b1e51fbfcb0fc4856972b89
stefanpython <stefanhtml@yahoo.com>
2024-04-02 15:39:55 +0300

    Link each post based on id

--- app/posts/page.tsx
+++ app/posts/page.tsx
@@ -1,14 +1,17 @@
 import { posts } from "@/app/lib/placeholder-data";
+import Link from "next/link";
 
 export default function Posts() {
   return (
     <div className="container max-w-2xl m-auto px-4 py-10">
       {posts.map((post) => (
-        <div key={post.id} className="post py-4">
-          <h3 className="text-2xl font-medium">{post.title}</h3>
-          <p>{post.description}</p>
-          <span className="flex text-gray-500">{post.date}</span>
-        </div>
+        <Link href={`/posts/${post.id}`}>
+          <div key={post.id} className="post py-4">
+            <h3 className="text-2xl font-medium">{post.title}</h3>
+            <p>{post.description}</p>
+            <span className="flex text-gray-500">{post.date}</span>
+          </div>
+        </Link>
       ))}
     </div>
   );
